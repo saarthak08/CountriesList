0ab838f4109a68736d1558d890df1a776f6a92e4
Saarthak <saarthakgupta08@gmail.com>
2019-03-25 17:39:18 +0530

    Added model classes for retrofit

--- app/src/main/java/com/sg/view/MainActivity.java
+++ app/src/main/java/com/sg/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.sg;
+package com.sg.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sg.R;
 
 public class MainActivity extends AppCompatActivity {
 
